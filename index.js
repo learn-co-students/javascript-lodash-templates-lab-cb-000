3ed650dfcaa3fb38afb2b3469434c162a0a00a15
Tyler Bristol <tahbristol@gmail.com>
2017-09-19 19:08:27 +0000

    update

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function createPost() {
-  var author = $('#author')[0].value;
-  var post = $('#post')[0].value;
+  var title = $('#title');
+  var content = $('#content');
+  var author = $('#author');
 
+  var postTemplate = $('#post-template');
+  var templateFn = _.template(postTemplate);
+  //var headerDiv = $('.header');
+  //var bodyDiv = $('.body');
+  //var footerDiv = $('.footer');
+  var postDiv = $('#post');
+  var templateHTML = templateFn({
+    'title': title,
+    'content': content,
+    'author': author
+  });
+  postDiv.innerHTML += templateHTML;
 
 }
