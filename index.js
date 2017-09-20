f9cf336e485e18002749432b42797ac789fb2032
Tyler Bristol <tahbristol@gmail.com>
2017-09-20 04:31:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,56 @@
 function createPost() {
-  var title = $('#title');
-  var content = $('#content');
-  var author = $('#author');
 
-  var postTemplate = $('#post-template');
-  var templateFn = _.template(postTemplate);
-  //var headerDiv = $('.header');
-  //var bodyDiv = $('.body');
-  //var footerDiv = $('.footer');
-  var postDiv = $('#post');
-  var templateHTML = templateFn({
+
+  var title = document.getElementById('title').value; //$('#title');
+  var content = document.getElementById('content').value; //$('#content');
+  var author = document.getElementById('author').value; //$('#author');
+
+
+  var postTemplate = document.getElementById('post-template').innerHTML; //$('#post-template');
+  var pageTemplate = document.getElementById('page-template').innerHTML;
+
+  var pageTemplateFn = _.template(pageTemplate);
+  var postTemplateFn = _.template(postTemplate);
+
+  var postDiv = document.getElementById('posts'); //$('#post');
+
+  var pageTemplateHTML = pageTemplateFn({
+    'post': postTemplate
+  });
+
+  var postTemplateHTML = postTemplateFn({
     'title': title,
     'content': content,
     'author': author
   });
-  postDiv.innerHTML += templateHTML;
+
+
+
+  postDiv.innerHTML += postTemplateHTML; // templateHTML;
+
+
+}
+
+function postComment() {
+
+  var comment = document.getElementById('comment').value;
+  var by = document.getElementById('commenter').value;
+
+
+  var commentTemplate = document.getElementById('comment-template').innerHTML;
+  var commentsTemplate = document.getElementById('comments-template').innerHTML;
+
+  var commentTemplateFn = _.template(commentTemplate);
+  var commentsTemplateFn = _.template(commentsTemplate);
+
+  var commentsTemplateHTML = commentsTemplateFn({
+    'comment': commentTemplate
+  });
+  var postCommentHTML = commentTemplateFn({
+    'comment': comment,
+    'commenter': by
+  });
+  var comments = document.getElementById('comments');
+  comments.innerHTML += commentsTemplateHTML;
 
 }
