97df664bf81da4fad777ce51bfa15bd8df73d4e0
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-23 21:15:25 +0000

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,31 @@ function createPost() {
   var pageTemp = document.getElementById('page-template').innerHTML;
   var pageTempFn = _.template(pageTemp);
   var pageTempHTML = pageTempFn();
-  console.log(pageTempHTML);
   var pageDiv = document.getElementById('page');
   pageDiv.innerHTML += pageTempHTML;
+
+  //Build post
+  var postTemp = document.getElementById('post-template').innerHTML;
+  var postTempFn = _.template(postTemp);
+  var postTempHTML = postTempFn({'title': title, 'body': body, 'author': author});
+  var postDiv = document.getElementById('post');
+  postDiv.innerHTML += postTempHTML;
+
+  //Build comments section
+  var commentsTemp = document.getElementById('comments-template').innerHTML;
+  var commentsTempFn = _.template(commentsTemp);
+  var commentsTempHTML = commentsTempFn();
+  var pageDiv = document.getElementById('page');
+  pageDiv.innerHTML += commentsTempHTML;
+}
+
+function postComment() {
+  var comment = document.getElementById('comment').value;
+  var commenter = document.getElementById('commenter').value;
+
+  var commentTemp = document.getElementById('comment-template').innerHTML;
+  var commentTempFn = _.template(commentTemp);
+  var commentTempHTML = commentTempFn({'comment': comment, 'commenter': commenter});
+  var commentsDiv = document.getElementById('comments');
+  commentsDiv.innerHTML += commentTempHTML;
 }
