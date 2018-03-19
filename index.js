9cf28a96383fed75f53dfa48dd535cc9e85eabef
Heather Christman <efl7a@yahoo.de>
2018-03-19 02:40:09 +0000

    actually got it work

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-// function loadPage () {
-//   var sidebar = "sidebar"
-//   var post = "Posts"
-//   var comments = "Comments"
-//
-//   var pageTemplate = document.getElementById("page-template").innerHTML;
-//   var pageTemplateFn = _.template(pageTemplate);
-//
-//   var pageElement = document.getElementById("page");
-//   var pageTemplateHTML = pageTemplateFn({"sidebar": sidebar, "post": post, "comments": comments});
-//
-//   pageElement.innerHTML += pageTemplateHTML
-// }
+function loadPage () {
+  var pageTemplate = document.getElementById("page-template").innerHTML;
+
+  var pageTemplateFn = _.template(pageTemplate);
+
+  var pageElement = document.getElementById("page");
+
+  pageElement.innerHTML += pageTemplateFn();
+}
+
+function loadComments () {
+  var commentsTemplate = document.getElementById("comments-template").innerHTML;
+
+  var commentsTemplateFn = _.template(commentsTemplate);
+
+  var commentsElement = document.getElementById("comments");
+
+  commentsElement.innerHTML += commentsTemplateFn();
+}
 // document.addEventListener("DOMContentLoaded", function(event) {
 //   loadPage()
 // })
@@ -18,38 +24,35 @@
 var sidebar = "Sidebar";
 
 function createPost () {
+  loadPage()
+
   var title = document.getElementById("postTitle").value;
   var text = document.getElementById("postBody").value;
   var author = document.getElementById("postAuthor").value;
 
   var postTemplate = document.getElementById("post-template").innerHTML;
-  var pageTemplate = document.getElementById("page-template").innerHTML;
 
   var templateFn = _.template(postTemplate);
-  var pageTemplateFn = _.template(pageTemplate);
 
   var templateHTML = templateFn({'title': title, 'post': text, 'author': author});
-  var pagetemplateHTML = pageTemplateFn({'sidebar': sidebar, 'post': templateHTML, "comments": " "});
 
-  var pageDiv = document.getElementById('page').innerHTML;
+  var postDiv = document.getElementById('post');
 
-  pageDiv.innerHTML += pagetemplateHTML;
+  postDiv.innerHTML += templateHTML;
+  loadComments();
 };
 
 function postComment() {
-  var commenter = document.getElementById("commenterName");
-  var comment = document.getElementById("commentText");
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
 
   var commentTemplate = document.getElementById("comment-template").innerHTML;
-  var pageTemplate = document.getElementById("page-template").innerHTML;
 
   var templateFn = _.template(commentTemplate);
-  var pageTemplateFn = _.template(pageTemplate);
 
   var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
-  var pagetemplateHTML = pageTemplateFn({"sidebar": sidebar, 'post': " ", "comments": templateHTML});
 
-  var pageDiv = document.getElementById('page').innerHTML;
+  var commentDiv = document.getElementById('comment');
 
-  pageDiv.innerHTML += pagetemplateHTML;
+  commentDiv.innerHTML += templateHTML;
 };
