822ad979ecfb9861ab1faac6c843d0646d74d36b
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-01 23:39:22 +0000

    Done.

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ function createPost(){
   var title = document.getElementById('title').value
   var body = document.getElementById('body').value
   var author = document.getElementById('author').value
-  var postTemplate = document.getElementById('post-template').innerHTML
+
+  var postTemplate = document.getElementById('post-form').innerHTML
   var templateFn = _.template(postTemplate)
 
-  var postsDiv = document.getElementById('create-post')
+  var postsDiv = document.getElementById('post')
 
   var templateHTML = templateFn({'title': title,
   'body': body, 'author': author})
