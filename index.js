46d0009f40c2f0c58d2977daf5c8357eeba4bdba
Chris Franklin <cf73021@gmail.com>
2019-02-04 16:32:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+function createPost(){
+  var title = document.getElementById("postTitle").value;
+  var author = document.getElementById("authorName").value;
+  var post = document.getElementById("postText").value;
+
+  var pageTemplate = document.getElementById("page-template").innerHTML;
+  var postTemplate = document.getElementById("post-template").innerHTML;
+  var commentsTemplate = document.getElementById("comments-template").innerHTML;
+
+  //create template functions
+  var pageTemplateFn = _.template(pageTemplate);
+  var postTemplateFn = _.template(postTemplate);
+  var commentsTemplateFn = _.template(commentsTemplate)
+
+  var postDiv = document.getElementById("page");
+
+  //execute template function with JSON object for the interpolated values
+  var pageTemplateHTML = pageTemplateFn({});
+  var commentsTemplateHTML = commentsTemplate({});
+  var postTemplateHTML = postTemplateFn({ 'title': title, 'post': post, 'author': author});
+
+  postDiv.innerHTML = pageTemplateHTML;
+  postDiv.innerHTML += postTemplateHTML;
+  postDiv.innerHTML += commentsTemplateHTML;
+}
+
+function postComment(){
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+
+  var commentTemplate = document.getElementById("comment-template").innerHTML;
+
+  //create template functions
+  var templateFn = _.template(commentTemplate);
+
+  var commentsDiv = document.getElementById("comments");
+
+  //execute template function with JSON object for the interpolated values
+  var templateHTML = templateFn({'comment': comment, 'commenter': commenter});
+
+  commentsDiv.innerHTML += templateHTML
+}
